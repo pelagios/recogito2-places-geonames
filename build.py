229ca1356dce5f0f289058d39467cd44a6056603
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-05 08:38:40 +0200

    Alternate names download

--- build.py
+++ build.py
@@ -2,14 +2,16 @@ import os
 from scripts.convertCountries import convertCountries
 from scripts.convertAdmin1 import convertAdmin1
 from scripts.convertCities import convertCities
+from scripts.loadNames import loadNames
 
 try:
     os.remove('geonames.jsonl')
 except OSError:
     pass
 
+loadNames()
 print('Starting conversion...')
-convertCountries()
-convertAdmin1()
-convertCities()
+#convertCountries()
+#convertAdmin1()
+#convertCities()
 print('Done.')
--- scripts/loadNames.py
+++ scripts/loadNames.py
@@ -1,9 +1,22 @@
+import os.path
 import urllib.request
+import zipfile
 
 ###
 # Downloads & unpacks the alternateNames.zip file from GeoNames (if needed)
 # and parses it into a table GeoNames ID -> names/languages
 ###
-def fetchIfNeeded():
-    url = 'http://download.geonames.org/export/dump/alternateNames.zip'
-    urllib.request.urlretrieve(url, 'data/alternateNames.zip')
+def fetchAndUnpackIfNeeded():
+    if not os.path.isfile('data/alternateNames.zip'):
+        print('Downloading alternatNames.zip from GeoNames')
+        url = 'http://download.geonames.org/export/dump/alternateNames.zip'
+        urllib.request.urlretrieve(url, 'data/alternateNames.zip')
+
+    if not os.path.isfile('data/alternateNames.txt'):
+        print('Unpacking')
+        z = zipfile.ZipFile('data/alternateNames.zip', 'r')
+        z.extractall('data')
+        z.close()
+
+def loadNames():
+    fetchAndUnpackIfNeeded()
