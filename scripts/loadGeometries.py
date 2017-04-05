5e92be61b436563ddb1984a84716dab3eebc2d5f
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-05 10:28:55 +0200

    Minor edit

--- scripts/loadGeometries.py
+++ scripts/loadGeometries.py
@@ -1,20 +1,24 @@
 import json
+import shapely
+from shapely.geometry import mapping, shape
 import gzip
 
 geometries = {}
 
-# Many GeoNames GeoJSON shapes don't follow the right-hand rule - fixing this here
-# def fixShapes():
-
 def loadGeometries():
     with gzip.open('data/shapes_simplified_low.txt.gz', 'rt') as f:
         print('Loading country border shapes')
-        for row in f:
+
+        # Skip header
+        it = iter(f)
+        next(it)
+
+        for row in it:
             fields = row.split('\t')
             try:
                 geometries[fields[0].strip()] = json.loads(fields[1].strip())
             except:
-                print("Skipping line: " + row)
+                print("Skipping line: ")
 
         f.close()
         print('Done.')
